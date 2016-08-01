d1812cc68c14e1bf340d519e8b3c8b1db074e077
Walter Mattingly <waltdakind@gmail.com>
2016-08-01 16:31:53 -0400

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,1 +1,11 @@
 // OK routes here
+
+var express = require('express');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+
