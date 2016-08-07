137a7f4ad4241d5c2d148c38336beaee3f2e02c4
Walter Mattingly <waltdakind@gmail.com>
2016-08-07 15:40:30 -0400

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -8,4 +8,27 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/test', function(req, res) {
+  res.send('');
+});
+
+app.get('/results', function(req, res) {
+  res.send('');
+});
+
+app.get('/api', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/api', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/api', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/api', function(req, res) {
+  res.send('Hello World!');
+});
 
